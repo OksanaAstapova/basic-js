65e00ffe01cd411caf8f368f1b8475024c7c7844
Oksana Astapova <astapova.frontend@gmail.com>
2022-05-02 21:59:00 +0300

    carbon

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,15 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+
+  if (typeof sampleActivity !== 'string' || !parseFloat(sampleActivity) || parseFloat(sampleActivity) < 0 || parseFloat(sampleActivity) > 15 ) return false;
+
+  let sample = MODERN_ACTIVITY / parseFloat(sampleActivity);
+  let age = 0.693 / HALF_LIFE_PERIOD;
+
+  return Math.ceil( Math.log(sample) / age);
+
 }
 
 module.exports = {
